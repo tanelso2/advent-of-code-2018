922b1a38f64754ff3f30ecf4f0bf1ede664a0269
Thomas Nelson <tanelso2@gmail.com>
2018-12-12 20:25:57 -0800

    Part2 done

--- day7/day7.ml
+++ day7/day7.ml
@@ -40,10 +40,13 @@ let dependencies_done (complete: string list) (dependencies: string list): bool 
     List.length |>
     (=) 0
 
-let get_next_to_process (complete: string list) (dep_map: dependencyMap): string option =
+let get_available_to_process (complete: string list) (dep_map: dependencyMap): string list =
     Map.filter dep_map ~f:(dependencies_done complete) |>
     Map.keys |>
-    List.filter ~f:(fun x -> List.mem ~equal:(=) complete x |> not) |>
+    List.filter ~f:(fun x -> List.mem ~equal:(=) complete x |> not)
+
+let get_next_to_process (complete: string list) (dep_map: dependencyMap): string option =
+    get_available_to_process complete dep_map |>
     List.hd
 
 let part1 (_ : depends list) : string =
@@ -57,7 +60,57 @@ let part1 (_ : depends list) : string =
     String.concat |>
     String.rev
 
-let part2 (_ : depends list) : int = 0
+type worker = {
+    processing: string;
+    finish_time: int;
+}
+
+let divide_workers (workers: worker list) (current_time: int): (worker list * string list) =
+    let f { finish_time; _} = finish_time <= current_time in
+    let (fin, not_fin) = List.partition_tf ~f:f workers in
+    let fin_letters = List.map fin ~f:(fun {processing; _} -> processing) in
+    (not_fin, fin_letters)
+
+let get_new_letters (num: int) (in_progress: string list) (complete: string list) (dep_map: dependencyMap): string list =
+    get_available_to_process complete dep_map |>
+    List.filter ~f:(fun x -> List.mem ~equal:(=) in_progress x |> not) |>
+    (fun y -> List.take y num)
+
+
+let max_workers = 5
+let buffer = 60
+
+let get_finish_time (current_time: int) (letter: string): int =
+    let c_int = Char.of_string letter |> Char.to_int in
+    let a_int = Char.to_int 'A' in
+    (c_int - a_int + 1 + buffer) + current_time
+
+
+let get_new_workers (time: int) (letters: string list): worker list =
+    let f letter =
+        {
+            processing = letter;
+            finish_time = (get_finish_time time letter);
+        }
+    in
+    List.map letters ~f:f
+
+
+let part2 (_ : depends list) : int =
+    let dep_map = build_dependency_map input in
+    let rec step (time: int) (workers: worker list) (complete: string list): int =
+        let (not_fin, fin) = divide_workers workers time in
+        let num_workers = max_workers - (List.length not_fin) in
+        let curr_complete = List.append fin complete in
+        let in_progress = List.map not_fin ~f:(fun {processing; _} -> processing) in
+        let new_letters = get_new_letters num_workers in_progress curr_complete dep_map in
+        let new_workers = get_new_workers time new_letters in
+        let curr_workers = List.append not_fin new_workers in
+        if curr_workers = []
+        then time
+        else step (time + 1) curr_workers curr_complete
+    in
+    step 0 [] []
 
 let () =
   ExtLib.print (part1 input);
